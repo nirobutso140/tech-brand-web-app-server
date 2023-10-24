bd5d2abe1272682368cdc9f3386b2088895123be
nirobutso140 <nirobutso140@gmail.com>
2023-10-25 01:23:18 +0600

    create get api to find single product, and put api to update product

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
 
@@ -32,46 +32,95 @@ async function run() {
 
     const productsCollection = client.db('productsDB').collection('products');
     const cartDataCollection = client.db('productsDB').collection('cart')
-  
+
     app.post('/products', async (req, res) => {
       const newProduct = req.body;
       console.log(newProduct);
       const result = await productsCollection.insertOne(newProduct);
       res.send(result);
-  })
+    })
 
-  app.post('/mycart', async (req, res) => {
-    const myCart = req.body;
-    console.log(myCart);
-    const result = await cartDataCollection.insertOne(myCart);
-    res.send(result)
-})
+    app.post('/mycart', async (req, res) => {
+      const myCart = req.body;
+      console.log(myCart);
+      const result = await cartDataCollection.insertOne(myCart);
+      res.send(result)
+    })
 
-  
+    // app.post('/update/:id', async (req, res) => {
+    //   const myCart = req.body;
+    //   console.log(myCart);
+    // })
+
+
 
     app.get('/products', async (req, res) => {
-    const result  = await productsCollection.find().toArray();
-    res.send(result);
-    
-    // const result = await cursor.toArray();
-})
+      const result = await productsCollection.find().toArray();
+      res.send(result);
 
-  app.get('/products/:id', async (req, res) => {
-    const id = req.params.id;
-    const query = {
-      brand: id,
-    };
-    const result  = await productsCollection.find(query).toArray();
-    res.send(result);
-    
-    // const result = await cursor.toArray();
-  
-})
+      // const result = await cursor.toArray();
+    })
 
-// app.get("/products", async (req, res) => {
-//   const result = await productsCollection.find().toArray();
-//   res.send(result);
-// });
+
+    app.get('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        brand: id,
+      };
+      const result = await productsCollection.find(query).toArray();
+      res.send(result);
+
+      // const result = await cursor.toArray();
+
+    })
+
+    app.get('/productDetails/:_id', async (req, res) => {
+      const id = req.params._id;
+      const query = {
+          _id:  new ObjectId(id),
+      };
+      const result = await productsCollection.find(query).toArray();
+      res.send(result);
+      
+    })
+
+    app.get('/update/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {
+          _id:  new ObjectId(id),
+      };
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+      
+    })
+
+    app.put('/updated/:id', async (req, res) => {
+      const id = req.params.id;
+      const result = req.body
+      console.log(id, result);
+      const filter = {_id : new ObjectId(id)}
+
+      const updatedProduct = {
+        $set: {
+          name : result.name,
+          brand: result.brand,
+          type: result.type,
+          price: result.price,
+          description: result.description,
+          rating: result.rating,
+          photo: result.photo
+        }
+      }
+      
+        const sendUpdatedProduct = await productsCollection.updateOne(filter, updatedProduct)
+        res.send(sendUpdatedProduct)
+    })
+
+
+    // app.get("/products", async (req, res) => {
+    //   const result = await productsCollection.find().toArray();
+    //   res.send(result);
+    // });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -86,10 +135,10 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!!!!!!!!!!!')
+  res.send('Hello World!!!!!!!!!!!')
 })
 
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
